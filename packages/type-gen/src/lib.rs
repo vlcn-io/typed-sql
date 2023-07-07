c70130f5de45bfacd6b85b5a1d2f1fad9aa4c59b
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-07 14:53:46 -0400

    stub for get_records_shape

--- packages/type-gen/src/lib.rs
+++ packages/type-gen/src/lib.rs
@@ -1,9 +1,27 @@
+use js_sys::Array;
 use sqlite3_parser::lexer::sql::Parser;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn get_record_shapes(ddl: String) -> Array {
+    let records = get_record_shapes_impl(ddl);
+    let arr = Array::new_with_length(10);
+    arr
+    // for i in 0..arr.length() {
+    //     let s = JsValue::from_str(&format!("str {}", i));
+    //     arr.set(i, s);
+    // }
+    // arr
+}
+
+type RecordName = String;
+type PropertyName = String;
+type PropertyType = String;
+type Property = (PropertyName, PropertyType);
+type Record = (RecordName, Vec<Property>);
+
+fn get_record_shapes_impl(ddl: String) -> Vec<(RecordName, Record)> {
+    vec![]
 }
 
 #[cfg(test)]
@@ -12,7 +30,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        get_record_shapes_impl("".to_string());
     }
 }
