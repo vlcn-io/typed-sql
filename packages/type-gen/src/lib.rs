64158fccfa2becd6a9924a13fb6fa47e83c00a79
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-07 15:57:43 -0400

    work around all the serialization madness. serde to the rescue

--- packages/type-gen/src/lib.rs
+++ packages/type-gen/src/lib.rs
@@ -1,5 +1,4 @@
 use fallible_iterator::FallibleIterator;
-use js_sys::Array;
 use sqlite3_parser::{
     ast::{Cmd, ColumnDefinition, CreateTableBody, Stmt},
     lexer::sql::{Error, Parser},
@@ -7,46 +6,20 @@ use sqlite3_parser::{
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn get_record_shapes(ddl: String) -> Array {
+pub fn get_record_shapes(ddl: String) -> Result<JsValue, JsValue> {
     if let Ok(records) = get_record_shapes_impl(ddl) {
-        let ret = Array::new_with_length(records.len() as u32);
-        for (i, record) in records.into_iter().enumerate() {
-            let js_record = Array::new_with_length(2);
-            js_record.set(0, JsValue::from_str(&record.0));
-            ret.set(i as u32, js_record);
-        }
-        ret
+        Ok(serde_wasm_bindgen::to_value(&records)?)
     } else {
         // todo: pass error string back
-        Array::new_with_length(0)
+        Ok(JsValue::NULL)
     }
 }
 
 type RecordName = String;
 type PropertyName = String;
 type PropertyType = Option<String>;
+type Property = (PropertyName, PropertyType);
 type Record = (RecordName, Vec<Property>);
-
-// wasm_bindgen does not yet support tuples so we have to structify all the things
-#[wasm_bindgen]
-pub struct Property {
-    name: String,
-    prop_type: Option<String>,
-}
-#[wasm_bindgen]
-impl Property {
-    // nor does it support string type directly without going through getters and setters
-    // https://github.com/rustwasm/wasm-bindgen/issues/1775
-    #[wasm_bindgen(getter)]
-    pub fn name(&mut self) -> String {
-        self.name
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn prop_type(&mut self) -> Option<String> {
-        self.prop_type
-    }
-}
 
 fn get_record_shapes_impl(ddl: String) -> Result<Vec<Record>, Error> {
     let mut parser = Parser::new(ddl.as_bytes());
@@ -90,10 +63,7 @@ fn get_properties(body: CreateTableBody) -> Vec<Property> {
 
 // Faithfully return types as specified. The layer above us (TS or Java or ...) will map to their native types.
 fn column_as_property(column: ColumnDefinition) -> Property {
-    Property {
-        name: column.col_name.0,
-        prop_type: column.col_type.map(|t| t.name),
-    }
+    (column.col_name.0, column.col_type.map(|t| t.name))
 }
 
 #[cfg(test)]
@@ -102,6 +72,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        get_record_shapes_impl("".to_string());
+        get_record_shapes_impl("".to_string()).unwrap();
     }
 }
