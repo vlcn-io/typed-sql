ff5ad8c0903c0c5574c0c0cd74706e88208268da
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-08 06:52:25 -0400

    remove scratch code, move runtime code into its own package

---
+++ packages/runtime/src/index.ts
@@ -0,0 +1,38 @@
+type Opaque<BaseType, BrandType = unknown> = BaseType & {
+  readonly [Symbols.base]: BaseType;
+  readonly [Symbols.brand]: BrandType;
+};
+
+namespace Symbols {
+  export declare const base: unique symbol;
+  export declare const brand: unique symbol;
+  export declare const internal: unique symbol;
+}
+
+type SchemaType<TSchema> = {
+  sql<TResult>(strings: TemplateStringsArray, ...values: any[]): Query<TResult>;
+  __content: string;
+  __type: TSchema;
+};
+
+export type ExtractResultType<P> = P extends Query<infer T> ? T : never;
+export type Query<T> = Opaque<string, T>;
+export type RecordTypes<P> = P extends SchemaType<infer T> ? T : never;
+export function declareSchema<TSchema>(strings: TemplateStringsArray, ...values: any[]): SchemaType<TSchema> {
+  const content = processTemplateStrings(strings, values);
+  return {
+    sql<T>(strings: TemplateStringsArray, ...values: any[]): Query<T> {
+      return processTemplateStrings(strings, values) as Query<T>;
+    },
+    __content: content,
+    __type: null as TSchema,
+  };
+}
+
+function processTemplateStrings(strings: TemplateStringsArray, values: any[]) {
+  let str = '';
+  strings.forEach((string, i) => {
+    str += string + (values[i] || '');
+  });
+  return str;
+}
--- packages/sandbox/src/index.ts
+++ packages/sandbox/src/index.ts
@@ -1,58 +1,4 @@
-// schema.sql<ZOMG>`Hey!`;
-
-export type Opaque<BaseType, BrandType = unknown> = BaseType & {
-  readonly [Symbols.base]: BaseType;
-  readonly [Symbols.brand]: BrandType;
-};
-
-namespace Symbols {
-  export declare const base: unique symbol;
-  export declare const brand: unique symbol;
-  export declare const internal: unique symbol;
-}
-
-export type Query<T> = Opaque<string, T>;
-type ExtractResultType<P> = P extends Query<infer T> ? T : never;
-type RecordTypes<P> = P extends SchemaType<infer T> ? T : never;
-type SchemaType<TSchema> = {
-  sql<TResult>(strings: TemplateStringsArray, ...values: any[]): Query<TResult>;
-  __content: string;
-  __type: TSchema;
-};
-
-function declareSchema<TSchema>(strings: TemplateStringsArray, ...values: any[]): SchemaType<TSchema> {
-  let str = '';
-  strings.forEach((string, i) => {
-    str += string + (values[i] || '');
-  });
-  return {
-    sql<T>(strings: TemplateStringsArray, ...values: any[]): Query<T> {
-      let str = '';
-      strings.forEach((string, i) => {
-        str += string + (values[i] || '');
-      });
-      return str as Query<T>;
-    },
-    __content: str,
-    __type: null as TSchema,
-  };
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import {declareSchema, RecordTypes} from "@vlcn.io/typed-sql";
 
 const schema = declareSchema<{
   foo: {
@@ -73,3 +19,4 @@ CREATE TABLE bar (id TEXT, name TEXT, weight FLOAT);
 type Records = RecordTypes<typeof schema>;
 
 // const x = '';
+// schema.sql<ZOMG>`Hey!`;
