9b435a6aa70e1b129b85ed243ca7bdbc1ba47b60
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-13 10:47:01 -0400

    handle literal types and named types

--- packages/type-gen/src/types.rs
+++ packages/type-gen/src/types.rs
@@ -14,6 +14,11 @@ pub enum BuiltinType {
     Quoted,
     BigInt,
     Unspecified,
+    Date,
+    Time,
+    CurrentDate,
+    CurrentTime,
+    CurrentTimestamp,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -33,37 +38,73 @@ pub enum Constraint {
 }
 pub type RelationName = String;
 pub type ColName = String;
-pub type ColType = Vec<(TypeKind, String)>;
+pub type ColType = Vec<(TypeKind, Option<BuiltinType>, Option<String>)>;
 pub type Col = (ColName, ColType);
 pub type Relation = (Option<RelationName>, Vec<Col>);
 pub type NamedRelation = (RelationName, Vec<Col>);
 
-pub fn builtin_type_string(c: BuiltinType) -> String {
-    match c {
-        BuiltinType::Blob => "blob".to_string(),
-        BuiltinType::Boolean => "boolean".to_string(),
-        BuiltinType::Json => "json".to_string(),
-        BuiltinType::Number => "number".to_string(),
-        BuiltinType::String => "string".to_string(),
-        BuiltinType::Float => "float".to_string(),
-        BuiltinType::Int => "int".to_string(),
-        BuiltinType::Any => "any".to_string(),
-        BuiltinType::Null => "null".to_string(),
-        BuiltinType::Quoted => "quoted_literal".to_string(),
-        BuiltinType::BigInt => "bigint".to_string(),
-        BuiltinType::Unspecified => "unspecified".to_string(),
-    }
-}
+// pub fn builtin_type_string(c: BuiltinType) -> String {
+//     match c {
+//         BuiltinType::Blob => "blob".to_string(),
+//         BuiltinType::Boolean => "boolean".to_string(),
+//         BuiltinType::Json => "json".to_string(),
+//         BuiltinType::Number => "number".to_string(),
+//         BuiltinType::String => "string".to_string(),
+//         BuiltinType::Float => "float".to_string(),
+//         BuiltinType::Int => "int".to_string(),
+//         BuiltinType::Any => "any".to_string(),
+//         BuiltinType::Null => "null".to_string(),
+//         BuiltinType::Quoted => "quoted_literal".to_string(),
+//         BuiltinType::BigInt => "bigint".to_string(),
+//         BuiltinType::Unspecified => "unspecified".to_string(),
+//         BuiltinType::Date => "date".to_string(),
+//         BuiltinType::Time => "time".to_string(),
+//     }
+// }
 
 pub fn builtin_type(c: BuiltinType) -> ColType {
-    return vec![(TypeKind::Builtin, builtin_type_string(c))];
+    return vec![(TypeKind::Builtin, Some(c), None)];
 }
 
 // This type name could be:
 // 1. A SQLite type
 // 2. Some string the user injected
 pub fn type_from_type_name(type_name: String) -> ColType {
-    vec![]
+    let lowered = type_name.to_lowercase();
+
+    // ugh........... SQLite and your weird data typing rule.
+    // https://www.sqlite.org/datatype3.html
+    // So we could follow the SQLite affinity rules but affinity is really a thing at the storage layer.
+    // A user that declares a custom type `International` would get affinity `INTEGER` which is bogus for us to return
+    // as a type. We'd want to return the custom `International` type back out.
+    // Yes, `create table bee (a INTERNATIONAL);` is valid SQLite.
+    // I think we'll need to take a departure from SQLite here and only treat
+    // `NULL`, `INTEGER`, `REAL`, `TEXT`, `BLOB` as builtins. The rest get assigned `CUSTOM`
+    // this'll need a lot of iteration since the SQLite type interface is so undefined.
+    if lowered == "integer" || lowered == "int" {
+        builtin_type(BuiltinType::Int)
+    } else if lowered == "biginteger" || lowered == "bigint" {
+        builtin_type(BuiltinType::BigInt)
+    } else if lowered == "text" || lowered == "string" {
+        builtin_type(BuiltinType::String)
+    } else if lowered == "number" {
+        builtin_type(BuiltinType::Number)
+    } else if lowered == "boolean" {
+        builtin_type(BuiltinType::Boolean)
+    } else if lowered == "blob" {
+        builtin_type(BuiltinType::Blob)
+    } else if lowered == "json" {
+        // TODO: allow user to reference shapes for json def?
+        builtin_type(BuiltinType::Json)
+    } else if lowered == "float" || lowered == "double" || lowered == "real" {
+        builtin_type(BuiltinType::Float)
+    } else if lowered == "date" {
+        builtin_type(BuiltinType::Date)
+    } else if lowered == "time" || lowered == "timestamp" {
+        builtin_type(BuiltinType::Time)
+    } else {
+        vec![(TypeKind::Custom, None, Some(type_name))]
+    }
 }
 
 // pub type ComplexRecord = (RecordName, Vec<ComplexProperty>);
