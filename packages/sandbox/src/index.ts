7e8580b2ab1e6b15fa1aeeec819cbc857f8dc386
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-14 10:11:12 -0400

    add license file

--- packages/sandbox/src/index.ts
+++ packages/sandbox/src/index.ts
@@ -1,26 +1,19 @@
 import { createSQL, type ResultOf, type SchemaOf } from "@vlcn.io/typed-sql";
 
 const sql = createSQL<{
-  person: {
-    id: number,
-    name: string | null
+  foo: {
+    a: number,
+    b: string | null,
+    c: bigint
   },
-  pet: {
-    id: number,
-    owner_id: number,
-    name: string | null
+  bar: {
+    d: number | null,
+    e: number | null
   }
-}>(`CREATE TABLE person (id INTEGER PRIMARY KEY NOT NULL, name TEXT);
-CREATE TABLE pet (id INTEGER PRIMARY KEY NOT NULL, owner_id INTEGER NOT NULL, name TEXT)`);
+}>(`CREATE TABLE foo (a INTEGER NOT NULL, b TEXT, c BIGINT NOT NULL);
+CREATE TABLE bar (d INTEGER, e FLOAT)`);
 
 const query = sql<[{
-  id: number
-}]>`SELECT id FROM person`;
-
-// TODO: warn on ambiguous select
-const query2 = sql<[{
-  id: number,
-  name: string | null,
-  owner_id: number
-}]>`SELECT * FROM person JOIN pet ON pet.owner_id = person.id`;
-
+  d: number | null,
+  e: number | null
+}]>`SELECT * FROM (SELECT * FROM bar)`
