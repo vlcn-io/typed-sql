9d04dd1e7467923931fa61a0906992a5d66a0b88
Azarattum <azarattum@pm.me>
2023-08-24 14:29:39 +0700

    handle queue rejections

--- incubator/typed-db/src/queue.ts
+++ incubator/typed-db/src/queue.ts
@@ -2,7 +2,7 @@ export function queue<T>(resource: T): Queued<T> {
   const tasks: Set<Promise<any>> = new Set();
   return <R>(task: (resource: T) => R | Promise<R>) => {
     const current = tasks.size
-      ? Promise.all(tasks).then(() => task(resource))
+      ? Promise.allSettled(tasks).then(() => task(resource))
       : task(resource);
     if (!isPromise(current)) return current;
     tasks.add(current);
